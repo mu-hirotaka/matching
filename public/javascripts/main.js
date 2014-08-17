db4ec65094c3725cc4d7d70aeeee1cc7bc2072c2
hirop4 <hirop4@gmail.com>
2014-08-17 10:16:45 +0900

    Modify spinner layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,21 @@
 $(function() {
   var $leftPlayer = $('.left-battle-area');
+  var $rightPlayer = $('.right-battle-area');
+  var $spinner = $('#spinner');
   var leftImagePath = '/images/1.gif';
+  var rightImagePath = '/images/2.gif';
 
   $leftPlayer.hide();
   $leftPlayer.css("background-image", "url('" + leftImagePath + "')");
   $leftPlayer.addClass('player-image');
   $leftPlayer.show();
 
-  var $rightPlayer = $('.right-battle-area');
-  var rightImagePath = '/images/2.gif';
   $rightPlayer.hide();
 
   setTimeout(createTargetView, 1000);
 
   function createTargetView() {
+    $spinner.fadeOut('normal');
     $rightPlayer.css("background-image", "url('" + rightImagePath + "')");
     $rightPlayer.addClass('player-image');
     $rightPlayer.fadeIn('slow');
